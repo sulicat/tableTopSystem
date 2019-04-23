e84a242db0ca11ee43f3d9c68ae69ba12d078f0a
sulicat <suleiman.barakat@mavs.uta.edu>
2019-04-22 21:17:37 -0500

    king support and back eating

--- src/games/checkers/checkers.py
+++ src/games/checkers/checkers.py
@@ -4,6 +4,7 @@ import Graphics
 import pygame
 import numpy as np
 import misc
+import pprint
 
 class Checkers( Graphics.Game ):
     def __init__(self, name):
@@ -40,12 +41,19 @@ class Checkers( Graphics.Game ):
 
                                                                                                     
     #checking adjacent tiles for movement
-    def move(self, current_state, value, move, location, prev_kills = []):
+    def move(self, current_state, value, move, location, prev_kills = [], visited = []):
         #will return single value (0, 1, 2) meaning (green, red, yellow) for squares
         #will return array (moves) with possible movement options
         #final_move is subset of moves with a different color showing no more jumps
         temp_move = [move[0], move[1]]
         kill_pos = {}
+
+        add_to_v = True
+        for v in visited:
+            if( v[0] == location[0][0] and v[1] == location[0][1] ):
+                add_to_v = False
+        if add_to_v:
+            visited.append( [location[0][0],location[0][1]]  )
 
         #case where piece on same piece is in adjacent square
         if current_state[move[0], move[1]] == value:
@@ -85,10 +93,43 @@ class Checkers( Graphics.Game ):
 
                 move1 = [move[0] + offs, move[1] - 1]
                 move2 = [move[0] + offs, move[1] + 1]
-                move1, move2 = np.asarray(move1), np.asarray(move2)
-                m1, m2 = self.adjacent(move1), self.adjacent(move2)
-                if m1 == 1:
-                    status, new_moves, new_final_moves, new_kill_pos = self.move( current_state, value, move1, [move], prev_kills.copy() )
+                move3 = [move[0] - offs, move[1] - 1]
+                move4 = [move[0] - offs, move[1] + 1]
+
+                move1_2 = [move[0] + 2*offs, move[1] - 2]
+                move2_2 = [move[0] + 2*offs, move[1] + 2]
+                move3_2 = [move[0] - 2*offs, move[1] - 2]
+                move4_2 = [move[0] - 2*offs, move[1] + 2]
+
+                move1, move2, move3, move4  = np.asarray(move1), np.asarray(move2), np.asarray(move3), np.asarray(move4)
+                m1, m2, m3, m4 = self.adjacent(move1), self.adjacent(move2), self.adjacent(move3), self.adjacent(move4)
+
+
+                print( "move" )
+                print( move1_2 )
+                print( move2_2 )
+                print( move3_2 )
+                print( move4_2 )
+                print( "location" )
+                print( location )
+                print( "visited" )
+                print( visited )
+
+                use_m = [True, True, True, True]
+                for v in visited:
+                    if( v[0] == move1_2[0] and v[1] == move1_2[1] ):
+                        use_m[0] = False
+                    if( v[0] == move2_2[0] and v[1] == move2_2[1] ):
+                        use_m[1] = False
+                    if( v[0] == move3_2[0] and v[1] == move3_2[1] ):
+                        use_m[2] = False
+                    if( v[0] == move4_2[0] and v[1] == move4_2[1] ):
+                        use_m[3] = False
+
+
+
+                if m1 == 1 and use_m[0]:
+                    status, new_moves, new_final_moves, new_kill_pos = self.move( current_state, value, move1, [move], prev_kills.copy(), visited.copy() )
                     if( status == 2 ):
                         for m in new_moves:
                             moves.append(m)
@@ -96,9 +137,26 @@ class Checkers( Graphics.Game ):
                     for key, v in new_kill_pos.items():
                         kill_pos[ key ] = v
 
+                if m2 == 1 and use_m[1]:
+                    status, new_moves, new_final_moves, new_kill_pos = self.move( current_state, value, move2, [move], prev_kills.copy(), visited.copy() )
+                    if( status == 2 ):
+                        for m in new_moves:
+                            moves.append(m)
 
-                if m2 == 1:
-                    status, new_moves, new_final_moves, new_kill_pos = self.move( current_state, value, move2, [move], prev_kills.copy() )
+                    for key, value in new_kill_pos.items():
+                        kill_pos[ key ] = value
+
+                if m3 == 1 and use_m[2]:
+                    status, new_moves, new_final_moves, new_kill_pos = self.move( current_state, value, move3, [move], prev_kills.copy(), visited.copy() )
+                    if( status == 2 ):
+                        for m in new_moves:
+                            moves.append(m)
+
+                    for key, value in new_kill_pos.items():
+                        kill_pos[ key ] = value
+
+                if m4 == 1 and use_m[3]:
+                    status, new_moves, new_final_moves, new_kill_pos = self.move( current_state, value, move4, [move], prev_kills.copy(), visited.copy() )
                     if( status == 2 ):
                         for m in new_moves:
                             moves.append(m)
@@ -298,11 +356,21 @@ class Checkers( Graphics.Game ):
 
                     move1 = [location[0][0] + offs, location[0][1] - 1]
                     move2 = [location[0][0] + offs, location[0][1] + 1]
-                    move1, move2 = np.asarray(move1), np.asarray(move2)
-                    m1, m2 = self.adjacent(move1), self.adjacent(move2)
+                    move3 = [location[0][0] - offs, location[0][1] - 1]
+                    move4 = [location[0][0] - offs, location[0][1] + 1]
+                    move1, move2, move3, move4 = np.asarray(move1), np.asarray(move2), np.asarray(move3), np.asarray(move4)
+                    m1, m2, m3, m4 = self.adjacent(move1), self.adjacent(move2), self.adjacent(move3), self.adjacent(move4)
 
                     kpos_1 = {}
                     kpos_2 = {}
+                    kpos_3 = {}
+                    kpos_4 = {}
+
+                    is_king = False
+                    for k in self.kings:
+                        if( location[0][0] == k[0] and location[0][1] == k[1] ):
+                            is_king = True
+
 
                     #we now draw the squares for movement options if it is possible
                     if m1 == 1:
@@ -360,15 +428,50 @@ class Checkers( Graphics.Game ):
                                 misc.render_imageInCell( screen, self.purple_star_img, (7-fmov[1], fmov[0]) )
                             '''
 
-                            #print( "final moves" )
-                            #print( final_moves )
-                            #print( "moves" )
-                            #print( moves )
+                    if is_king:
+                        if m3 == 1:
+                            # pygame.draw.rect(screen, (0,255,0), ((7-move2[1])*cw, move2[0]*ch, cw, ch))
+                            x, moves, final_moves, kpos_3 = self.move(self.current_state, single_value, move3, location, [])
+
+                            if x == 0:
+                                r = 7-final_moves[1]
+                                c = final_moves[0]
+                                pygame.draw.rect(screen, (0,255,0), (r*cw, c*ch, cw/4, ch/4))
+                                pygame.draw.rect(screen, (0,255,0), ((r+1)*cw - (cw/4), (c+1)*ch - (cw/4), cw/4, ch/4))
+
+                            elif x==2:
+                                for mov in moves:
+                                    r = 7-mov[1]
+                                    c = mov[0]
+                                    misc.render_imageInCell( screen, self.purple_star_img, (r, c) )
+
+                        if m4 == 1:
+                            # pygame.draw.rect(screen, (0,255,0), ((7-move2[1])*cw, move2[0]*ch, cw, ch))
+                            x, moves, final_moves, kpos_4 = self.move(self.current_state, single_value, move4, location, [])
+
+                            if x == 0:
+                                r = 7-final_moves[1]
+                                c = final_moves[0]
+                                pygame.draw.rect(screen, (0,255,0), (r*cw, c*ch, cw/4, ch/4))
+                                pygame.draw.rect(screen, (0,255,0), ((r+1)*cw - (cw/4), (c+1)*ch - (cw/4), cw/4, ch/4))
+
+                            elif x==2:
+                                for mov in moves:
+                                    r = 7-mov[1]
+                                    c = mov[0]
+                                    misc.render_imageInCell( screen, self.purple_star_img, (r, c) )
+
+
+
 
                     kpos = {}
                     for k,v in kpos_1.items():
                         kpos[k] = v
                     for k,v in kpos_2.items():
+                        kpos[k] = v
+                    for k,v in kpos_3.items():
+                        kpos[k] = v
+                    for k,v in kpos_4.items():
                         kpos[k] = v
 
                     self.kill_positions = kpos.copy()
